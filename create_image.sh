d4290904e9c098cba58f070e0ce1920dc7551784
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-07-16 20:03:36 +0900

    change blas install to create_image

--- create_image.sh
+++ create_image.sh
@@ -80,3 +80,21 @@ ln -sf /usr/lib64/libsnappy.so.1 /root/hadoop-native/.
 echo '#!/bin/bash' > /usr/bin/realpath
 echo 'readlink -e "$@"' >> /usr/bin/realpath
 chmod a+x /usr/bin/realpath
+
+# install OpenBlas for fast mllib execution
+git clone https://github.com/xianyi/OpenBlas.git
+cd OpenBlas/
+make clean
+make -j4
+sudo mkdir /usr/lib64/OpenBLAS
+sudo chmod o+w,g+w /usr/lib64/OpenBLAS/
+make PREFIX=/usr/lib64/OpenBLAS install
+sudo rm /etc/ld.so.conf.d/atlas-x86_64.conf
+sudo ldconfig
+sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/libblas.so
+sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/libblas.so.3
+sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/libblas.so.3.5
+sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/liblapack.so
+sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/liblapack.so.3
+sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/liblapack.so.3.5
+cd ..
