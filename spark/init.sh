ca8be82059f2c7e5e4959e0111ef6eaf589389f3
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-07-16 18:10:33 +0900

    adding OpenBlas install commands for MLLib

--- spark/init.sh
+++ spark/init.sh
@@ -142,6 +142,24 @@ else
   tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
   rm spark-*.tgz
   mv `ls -d spark-* | grep -v ec2` spark
+
+  # install OpenBlas for fast mllib execution
+  git clone https://github.com/xianyi/OpenBlas.git
+  cd OpenBlas/
+  make clean
+  make -j4
+  sudo mkdir /usr/lib64/OpenBLAS
+  sudo chmod o+w,g+w /usr/lib64/OpenBLAS/
+  make PREFIX=/usr/lib64/OpenBLAS install
+  sudo rm /etc/ld.so.conf.d/atlas-x86_64.conf 
+  sudo ldconfig
+  sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/libblas.so
+  sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/libblas.so.3
+  sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/libblas.so.3.5
+  sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/liblapack.so
+  sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/liblapack.so.3
+  sudo ln -sf /usr/lib64/OpenBLAS/lib/libopenblas.so /usr/lib64/liblapack.so.3.5
+  cd ..
 fi
 
 popd > /dev/null
