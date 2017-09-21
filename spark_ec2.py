fc9b95d69b721c08bce28a4fb38a953852627f5f
Myungjun Son <mjs8612@gmail.com>
2017-09-21 15:15:34 +0900

    test ebs size-1.2

--- spark_ec2.py
+++ spark_ec2.py
@@ -702,7 +702,7 @@ def launch_cluster(conn, opts, cluster_name):
             num_slaves_this_zone = get_partition(opts.slaves, num_zones, i)
             block_map = BlockDeviceMapping()
             device = EBSBlockDeviceType();
-            device.size='68719476736'
+            device.size='64'
             device.volume_type="ebs"
             device.delete_on_termination= True
             block_map["/dev/xvda"]=device
