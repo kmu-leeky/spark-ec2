0924faf8ce6f4d6845c24b208377a2a07010a804
mjaysonnn <smj8612@kookmin.ac.kr>
2017-11-22 11:13:17 +0900

    adding i3 instance types(setting hvm)

--- spark_ec2.py
+++ spark_ec2.py
@@ -474,7 +474,13 @@ EC2_INSTANCE_TYPES = {
     "t2.medium":   "hvm",
     "t2.large":    "hvm",
     "t2.xlarge":   "hvm",
-    "t2.2xlarge":  "hvm"
+    "t2.2xlarge":  "hvm",
+    "i3.large":    "hvm",
+    "i3.xlarge":   "hvm",
+    "i3.2xlarge":  "hvm",
+    "i3.4xlarge":  "hvm",
+    "i3.8xlarge":  "hvm",
+    "i3.16xlarge": "hvm"
 }
 
 # As of Sep. 18th. 2017. on-demand in uw-west-2 
